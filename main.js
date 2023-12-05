4b47009622f2f06a82897b6eaa5b0c4c4482094d
jacioferton <jaciofferton@gmail.com>
2023-12-05 16:40:57 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,58 @@
+const data = [
+    {
+        id: 0,
+        nome: "Pastel de carne",
+        preco: 4.00,
+        foto: "",
+        categoria: "comida",
+        descricao: "Pastel de forno sabor carne"
+    },
+    {
+        id: 1,
+        nome: "Pastel de queijo",
+        preco: 4.00,
+        foto: "",
+        categoria: "comida",
+        descricao: "Pastel de forno sabor queijo"
+    },
+    {
+        id: 2,
+        nome: "Pastel misto",
+        preco: 4.00,
+        foto: "",
+        categoria: "comida",
+        descricao: "Pastel de forno sabor misto"
+    },
+    {
+        id: 3,
+        nome: "Coxinha",
+        preco: 4.00,
+        foto: "",
+        categoria: "comida",
+        descricao: "Coxinha de frango"
+    },
+    {
+        id: 4,
+        nome: "Enroladinho",
+        preco: 4.00,
+        foto: "",
+        categoria: "comida",
+        descricao: "Enroladinho de queijo e salsicha"
+    },
+    {
+        id: 5,
+        nome: "Caldo de cana",
+        preco: 4.00,
+        foto: "",
+        categoria: "bebida",
+        descricao: "700 ml"
+    },
+    {
+        id: 6,
+        nome: "Caldo de cana",
+        preco: 3.00,
+        foto: "",
+        categoria: "bebida",
+        descricao: "500 ml"
+    },
+]
